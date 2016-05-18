9a05a9461c1c5b2d2a70a108448cc34e4d16f676
jacabello <javachohair@gmail.com>
2016-05-18 11:12:56 -0400

    Add files via upload

---
+++ lang/en/giq-.php
@@ -0,0 +1,39 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+
+/**
+ * English strings for giq-
+ *
+ * You can have a rather longer description of the file as well,
+ * if you like, and it can span multiple lines.
+ *
+ * @package    mod_giq-
+ * @copyright  2015 Your Name
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['modulename'] = 'giq-';
+$string['modulenameplural'] = 'giq-s';
+$string['modulename_help'] = 'Use the giq- module for... | The giq- module allows...';
+$string['giq-fieldset'] = 'Custom example fieldset';
+$string['giq-name'] = 'giq- name';
+$string['giq-name_help'] = 'This is the content of the help tooltip associated with the giq-name field. Markdown syntax is supported.';
+$string['giq-'] = 'giq-';
+$string['pluginadministration'] = 'giq- administration';
+$string['pluginname'] = 'giq-';
